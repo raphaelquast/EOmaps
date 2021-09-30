eec27ae391d3c0908c7cbaf979769f4a5d0759a4
Raphael <raphael.quast@geo.tuwien.ac.at>
2021-09-30 20:26:52 +0200

    set version to 0.1 and publish-action to PyPi

--- mapit/__init__.py
+++ mapit/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0"
+__version__ = "0.1"
 __author__ = "Raphael Quast"
 
 from .mapit import MapIt
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mapit",
-    version=0.0,
+    version=0.1,
     description="a general-purpose library to plot and analyze large geographical datasets.",
     packages=find_packages(),
     package_dir={"mapit": "mapit"},
