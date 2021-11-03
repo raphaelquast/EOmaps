173dcbccf1c9917ee89637b304efc58f06c9b446
Raphael <raphael.quast@geo.tuwien.ac.at>
2021-11-03 16:38:35 +0100

    update version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ For COPYING and LICENSE details, please refer to the LICENSE file
 """
 from setuptools import setup, find_packages
 
-version = "0.1.6"
+version = "1.0"
 
 setup(
     name="EOmaps",
