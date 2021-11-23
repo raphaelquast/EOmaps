8091b29bed8a30c621523314ecd9bd36a51bd081
Raphael <raphael.quast@geo.tuwien.ac.at>
2021-11-23 13:14:36 +0100

    make sure we do not add self as a child!

--- eomaps/eomaps.py
+++ eomaps/eomaps.py
@@ -389,9 +389,10 @@ class Maps(object):
     @parent.setter
     def parent(self, parent):
         assert self._parent is None, "EOmaps: There is already a parent Maps object!"
-        self._parent = parent
-        # add the child to the topmost parent-object
-        self.parent._add_child(self)
+        if parent not in [self, None]:
+            self._parent = parent
+            # add the child to the topmost parent-object
+            self.parent._add_child(self)
 
     def join_limits(self, *args):
         """
