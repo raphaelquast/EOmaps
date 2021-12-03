f938a5b658a9e07e3000b0f0ad4cad25d8e3920a
Raphael Quast <raphael.quast@geo.tuwien.ac.at>
2021-12-03 16:48:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-version = "2.0.2"
+version = "2.1.0"
 
 setup(
     name="EOmaps",
