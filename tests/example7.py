0ec87962ba9696fab785b46603e5761854b05781
Raphael <raphael.quast@geo.tuwien.ac.at>
2022-01-25 23:09:53 +0100

    update example7 to use the new functionalities

--- tests/example7.py
+++ tests/example7.py
@@ -1,46 +1,22 @@
 # EOmaps example 7: Using geopandas - interactive shapes!
 
 from eomaps import Maps, MapsGrid
-from cartopy.feature import shapereader
-import geopandas as gpd
 import pandas as pd
 import numpy as np
+import geopandas as gpd
+
+# geopandas is used internally... the import is just here to show that!
+
+# While this example focusses on "m.add_feature", it works completely similar
+# with a custom GeoDataFrame by replacing "m.add_feature..." with "m.add_gdf()"
+# (the functionalities and arguments are completely similar)
+
 
 # ----------- create some example-data
 lon, lat = np.meshgrid(np.linspace(-180, 180, 25), np.linspace(-90, 90, 25))
 data = pd.DataFrame(
     dict(lon=lon.flat, lat=lat.flat, data=np.sqrt(lon ** 2 + lat ** 2).flat)
 )
-
-# ----------- load some data from NaturalEarth and put in in a geopandas.GeoDataFrame
-path = shapereader.natural_earth(
-    resolution="10m", category="cultural", name="admin_0_countries"
-)
-r = shapereader.Reader(path)
-gdf = gpd.GeoDataFrame(
-    dict(name=[i.attributes["NAME_EN"] for i in r.records()]),
-    geometry=[*r.geometries()],
-    crs=4326,
-)
-
-# -----------  define a callback to color picked objects
-def cb(self, ind, **kwargs):
-    if ind is not None:
-        # get the selected geometry and re-project it to the desired crs
-        geom = self.cb.pick["countries"].data.loc[[ind]].geometry
-        # add the geometry to the map
-        art = self.figure.ax.add_geometries(
-            geom, self.figure.ax.projection, fc="r", alpha=0.75
-        )
-        # make the geometry temporary (e.g. remove it on the next pick event)
-        self.cb.pick["countries"].add_temporary_artist(art, layer=2)
-
-
-# -----------  define a callback to get the name of the picked object
-def txt(m, ID, val, pos, ind):
-    if ind is not None:
-        return gdf.loc[ind]["name"]
-
 
 # ----------- setup some maps objects and assign datasets and the plot-crs
 mg = MapsGrid(1, 2)
@@ -51,26 +27,28 @@ mg.m_0_1.set_data(data=data, xcoord="lon", ycoord="lat", crs=4326)
 mg.m_0_1.plot_specs.crs = Maps.CRS.Orthographic(45, 45)
 
 mg.add_feature.preset.ocean()
+mg.add_feature.cultural_50m.admin_0_countries(
+    picker_name="countries",
+    pick_method="contains",
+    val_key="NAME",
+    fc="none",
+    ec="k",
+    lw=0.5,
+)
 
 for m in mg:
     # plot the attached dataset and attach callbacks to it
     m.set_shape.rectangles(radius=3, radius_crs=4326)
     m.plot_map(alpha=0.75, ec=(1, 1, 1, 0.5), pick_distance=25, colorbar=False)
 
+    # attach a callback to highlite the rectangles
     m.cb.pick.attach.mark(
-        permanent=False, shape="rectangles", fc="none", ec="b", lw=2, layer=1
+        permanent=False, shape="rectangles", fc="none", ec="b", lw=2, layer=5
     )
 
-    # plot a geopandas GeoDataFrame and attach some callbacks to it
-    m.add_gdf(
-        gdf,
-        picker_name="countries",
-        fc="none",
-        lw=0.5,
-        zorder=10,
-    )
-    m.cb.pick["countries"].attach(cb)
-    m.cb.pick["countries"].attach.annotate(text=txt)
+    # attach a callback to highlite the countries and indicate the names
+    m.cb.pick["countries"].attach.highlight_geometry(fc="r", ec="k", lw=0.5)
+    m.cb.pick["countries"].attach.annotate(text=lambda val, **kwargs: str(val))
 
 
 mg.share_pick_events()  # share default pick events
