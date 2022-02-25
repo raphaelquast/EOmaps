9ac7f85c5719ee8b0d15034afe5a42fb4dd287c8
Raphael <raphael.quast@geo.tuwien.ac.at>
2022-02-25 14:07:49 +0100

    update version number to 3.1.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ try:
 except Exception:
     long_description = "A library to create interactive maps of geographical datasets."
 
-version = "3.1.3"
+version = "3.1.4"
 
 setup(
     name="EOmaps",
