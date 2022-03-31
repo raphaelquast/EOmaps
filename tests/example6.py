64a90a695253f483c571009ed765a6f904203dce
Raphael <raphael.quast@geo.tuwien.ac.at>
2022-03-31 22:50:32 +0200

    update example 6

--- tests/example6.py
+++ tests/example6.py
@@ -1,5 +1,6 @@
 # EOmaps example 6: WebMap services and layer-switching
 
+# %matplotlib widget
 from eomaps import Maps
 import numpy as np
 import pandas as pd
@@ -11,37 +12,42 @@ data = pd.DataFrame(
 )
 # --------------------------------
 
-m = Maps(Maps.CRS.GOOGLE_MERCATOR, layer="S1GBM_vv")
+m = Maps(Maps.CRS.GOOGLE_MERCATOR)
 # set the crs to GOOGLE_MERCATOR to avoid reprojecting the WebMap data
 # (makes it a lot faster and it will also look much nicer!)
 # ------------- LAYER 0
-# add S1GBM as a base-layer
-wms1 = m.add_wms.S1GBM
-wms1.add_layer.vv()
+# add a layer showing S1GBM
+m1 = m.new_layer(layer="S1GBM_vv")
+m1.add_wms.S1GBM.add_layer.vv()
 
 # ------------- LAYER 1
-# if you just want to add features, you can do it within the same Maps-object!
-# add OpenStreetMap on the currently invisible layer (1)
-wms2 = m.add_wms.OpenStreetMap  # .OSM_mundialis
-wms2.add_layer.default(layer="OSM")
+# if you just want to add features, you can also do it within the same Maps-object!
+# add OpenStreetMap on the currently invisible layer (OSM)
+m.add_wms.OpenStreetMap.add_layer.default(layer="OSM")
 
 # ------------- LAYER 2
-# create a connected maps-object and plot some data on a new layer (2)
+# create a new layer and plot some data
 m2 = m.new_layer(layer="data")
 m2.set_data(data=data.sample(5000), xcoord="lon", ycoord="lat", crs=4326)
 m2.set_shape.geod_circles(radius=20000)
 m2.plot_map()
 
-# ------------ CALLBACKS
-# on a left-click, show layer 1 in a rectangle (with a size of 20% of the axis)
-m.cb.click.attach.peek_layer(layer=["data", "OSM"], how=(0.2, 0.2))
+# add a callback that is only executed if the "data" layer is visible
+m2.cb.pick.attach.annotate(zorder=100)  # use a high zorder to put it on top
 
-# on a right-click, "swipe" layer (2) from the left
+# ------------ CALLBACKS
+# since m.layer == "all", the callbacks assigned to "m" will be executed on all layers!
+
+# on a left-click, show layers ("data", "OSM") in a rectangle
+# (with a size of 20% of the axis)
+m.cb.click.attach.peek_layer(layer=["data", "OSM"], how=0.2)
+
+# on a right-click, "swipe" the layers ("data", "S1GBM_vv") from the left
 m.cb.click.attach.peek_layer(
     layer=["data", "S1GBM_vv"], how="left", button=3, overlay=True
 )
 
-
+# switch between the layers with the keys 0, 1 and 2
 m.cb.keypress.attach.switch_layer(layer="S1GBM_vv", key="0")
 m.cb.keypress.attach.switch_layer(layer="OSM", key="1")
 m.cb.keypress.attach.switch_layer(layer="data", key="2")
@@ -53,11 +59,20 @@ m.figure.gridspec.update(left=0.01, right=0.99, bottom=0.01, top=0.99)
 m.add_logo()
 
 # add a utility-widget for switching the layers
-m.util.layer_selector(loc="upper left", ncol=3, bbox_to_anchor=(0.01, 0.99))
+m.util.layer_selector(
+    loc="upper left",
+    ncol=3,
+    bbox_to_anchor=(0.01, 0.99),
+    layers=["OSM", "S1GBM_vv", "data"],
+)
 
 m.util.layer_slider(
     pos=(0.5, 0.93, 0.38, 0.025),
     color="r",
     handle_style=dict(facecolor="r"),
     txt_patch_props=dict(fc="w", ec="none", alpha=0.75, boxstyle="round, pad=.25"),
+    layers=["OSM", "S1GBM_vv", "data"],
 )
+
+# show the S1GBM layer on start
+m.show_layer("S1GBM_vv")
