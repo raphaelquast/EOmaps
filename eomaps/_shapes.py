fix ellipse masking and "m.indicate_masked_points()"

eomaps/_shapes.py
@@ -602,18 +602,19 @@ class shapes(object):
             # mask any point that is in a different quadrant than the center point
             maskx = pts_quadrants != quadrants[:, np.newaxis]
             # take care of points that are on the center line (e.g. don't mask them)
-            # (use a +- 10 degree around 0 as threshold)
+            # (use a +- 25 degree around 0 as threshold)
             cpoints = np.broadcast_to(
-                np.isclose(xp, xc, atol=10)[:, np.newaxis], xs.shape
+                np.isclose(xp, xc, atol=25)[:, np.newaxis], xs.shape
             )
 
             maskx[cpoints] = False
             xs.mask[maskx] = True
-
             ys.mask = xs.mask
 
-            mask = ~np.all(maskx, axis=1) & np.isfinite(theta)
-
+            # mask any datapoint that has less than 4 of the ellipse-points unmasked
+            mask = ~(
+                n - np.count_nonzero(xs.mask, axis=1) <= min(n / 2, 4)
+            ) & np.isfinite(theta)
             return xs, ys, mask
 
         def get_coll(self, x, y, crs, **kwargs):

eomaps/eomaps.py
@@ -3994,7 +3994,7 @@ class Maps(object):
             print("EOmaps: There are no masked points to indicate!")
             return
 
-        mask = np.broadcast_to(self._data_mask, self._props["z_data"].shape)
+        mask = self._data_mask.reshape(self._zshape)
 
         if len(self._props["z_data"][~mask]) == 0:
             print("EOmaps: There are no masked points to indicate!")
@@ -4002,13 +4002,15 @@ class Maps(object):
 
         kwargs.setdefault("ec", "r")
 
-        self.ax.scatter(
+        a = self.ax.scatter(
             self._props["x0"][~mask],
             self._props["y0"][~mask],
             cmap=self.classify_specs._cbcmap,
             c=self._props["z_data"][~mask],
             **kwargs,
         )
+
+        self.BM.add_bg_artist(a, layer=self.layer)
 
     @staticmethod
     def _make_rect_poly(x0, y0, x1, y1, crs=None, npts=100):