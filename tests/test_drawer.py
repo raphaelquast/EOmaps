b2c41e3e2a3092dce42dd21938bb629e809cc8e3
Raphael <raphael.quast@geo.tuwien.ac.at>
2023-02-10 16:33:54 +0100

    make sure figure is drawn before testing the drawer

--- tests/test_drawer.py
+++ tests/test_drawer.py
@@ -64,6 +64,7 @@ class TestDraw(unittest.TestCase):
     def test_basic_drawing_capabilities(self):
         m = Maps()
         m.add_feature.preset.coastline()
+        m.redraw()
         m.draw.rectangle(fc="none", ec="r")
         self.click_ax_center(m)
         self.click_ax_center(m, dx=20, dy=20, button=2)
