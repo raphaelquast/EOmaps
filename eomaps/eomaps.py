6ba211f636efe8ac180b70afca5ef4df7931ca28
Raphael <raphael.quast@geo.tuwien.ac.at>
2023-07-02 01:25:17 +0200

    minor

--- eomaps/eomaps.py
+++ eomaps/eomaps.py
@@ -106,7 +106,7 @@ def _handle_backends():
             plt.ion()
         else:
             plt.ioff()
-            _log.info(
+            _log.debug(
                 "EOmaps: matplotlib's interactive mode is turned off. "
                 "Call `m.show()` to show the map!"
             )
@@ -167,7 +167,7 @@ class _MapsMeta(type):
         This function must be called before initializing any :py:class:`Maps` object!
 
         >>> from eomaps import Maps
-        >>> Maps.global_config(always_on_top=True)
+        >>> Maps.config(always_on_top=True)
 
         (parameters set to None are NOT updated!)
 
@@ -3829,6 +3829,7 @@ class Maps(metaclass=_MapsMeta):
             _handle_backends()
 
             self._f = plt.figure(**kwargs)
+            _log.debug("EOmaps: New figure created.")
 
             # make sure we keep a "real" reference otherwise overwriting the
             # variable of the parent Maps-object while keeping the figure open
