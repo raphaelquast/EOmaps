dd024d3e7422e462d94ed0078bca48719ec1a246
Raphael <raphael.quast@geo.tuwien.ac.at>
2023-07-19 18:08:06 +0200

    add new "contour" shape

--- eomaps/eomaps.py
+++ eomaps/eomaps.py
@@ -3010,6 +3010,8 @@ class Maps(metaclass=_MapsMeta):
         if not self._inherit_classification:
             if self.classify_specs.scheme is not None:
                 _log.debug("EOmaps: Classifying...")
+            elif self.shape.name == "contour":
+                self.set_classify.EqualInterval(k=5)
 
         self._set_vmin_vmax(
             vmin=kwargs.pop("vmin", None), vmax=kwargs.pop("vmax", None)
@@ -4713,14 +4715,14 @@ class Maps(metaclass=_MapsMeta):
                 kwargs[key] = self._handle_explicit_colors(kwargs[key])
 
         # don't pass the array if explicit facecolors are set
-        if explicit_fc:
+        if explicit_fc and self.shape.name not in ["contour"]:
             args = dict(array=None, cmap=None, norm=None, **kwargs)
         else:
             args = dict(
                 array=props["z_data"], cmap=self._cbcmap, norm=self._norm, **kwargs
             )
 
-        if self.shape.name in ["raster"]:
+        if self.shape.name in ["raster", "contour"]:
             # if input-data is 1D, try to convert data to 2D (required for raster)
             # TODO make an explicit data-conversion function for 2D-only shapes
             if len(self._xshape) == 2 and len(self._yshape) == 2:
