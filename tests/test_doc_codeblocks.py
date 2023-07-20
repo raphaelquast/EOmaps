1158dcdbfb91b0ed7e8dae7d074084fa8af7d64e
Raphael <raphael.quast@geo.tuwien.ac.at>
2023-07-20 10:05:52 +0200

    fix doc code-block unittests

--- tests/test_doc_codeblocks.py
+++ tests/test_doc_codeblocks.py
@@ -3,6 +3,7 @@ from docutils.core import publish_doctree
 from docutils.utils import Reporter
 import matplotlib.pyplot as plt
 import unittest
+import os
 
 
 def gen_test(i, name, code):
@@ -19,41 +20,48 @@ def gen_test(i, name, code):
     return test
 
 
+def is_code_block(node):
+    return (
+        node.tagname == "literal_block"
+        and "code" in node.attributes["classes"]
+        and len(node.attributes["ids"]) > 0
+        and node.attributes["ids"][0].startswith("test")
+    )
+
+
 class _TestSequenceMeta(type):
     def __new__(mcs, name, bases, tests):
         # the path to the re-structured text file that should be analyzed
-        p = Path(__file__).parent.parent / "docs" / "api.rst"
+        parent_path = Path(__file__).parent.parent / "docs"
+        # set cwd to doc parent path to avoid issues with 'include' statements
+        cwd = os.getcwd()
+        os.chdir(parent_path)
 
-        with open(p, "r", encoding="utf8") as file:
-            data = file.read()
+        for p in filter(lambda x: x.suffix == ".rst", parent_path.iterdir()):
+            with open(p, "r", encoding="utf8") as file:
+                data = file.read()
 
-        # initialize a "Publisher" to parse the file
-        doctree = publish_doctree(
-            data, settings_overrides={"report_level": Reporter.SEVERE_LEVEL}
-        )
-
-        def is_code_block(node):
-            return (
-                node.tagname == "literal_block"
-                and "code" in node.attributes["classes"]
-                and len(node.attributes["ids"]) > 0
-                and node.attributes["ids"][0].startswith("test")
+            # initialize a "Publisher" to parse the file
+            doctree = publish_doctree(
+                data, settings_overrides={"report_level": Reporter.SEVERE_LEVEL}
             )
 
-        # get a list of all code-blocks in the file
-        # replace .traverse with .findall once docutils > 18.1 is used!
-        if hasattr(doctree, "findall"):
-            code_blocks = list(doctree.findall(condition=is_code_block))
-        else:
-            code_blocks = list(doctree.traverse(condition=is_code_block))
+            # get a list of all code-blocks in the file
+            # replace .traverse with .findall once docutils > 18.1 is used!
+            if hasattr(doctree, "findall"):
+                code_blocks = list(doctree.findall(condition=is_code_block))
+            else:
+                code_blocks = list(doctree.traverse(condition=is_code_block))
 
-        # generate unique tests for each code snippet
-        for i, node in enumerate(code_blocks):
-            source_code = node.astext()
-            name = node.attributes["names"][0]
+            # generate unique tests for each code snippet
+            for i, node in enumerate(code_blocks):
+                source_code = node.astext()
+                name = node.attributes["names"][0]
 
-            test_name = f"test_{i}"
-            tests[test_name] = gen_test(i, name, source_code)
+                test_name = f"test_{i}"
+                tests[test_name] = gen_test(i, name, source_code)
+
+        os.chdir(cwd)
 
         return type.__new__(mcs, name, bases, tests)
 
