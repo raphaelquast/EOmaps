82a6f9b79ef7eab390fb84713e08b7c449c0b1db
Raphael Quast <raphael.quast@geo.tuwien.ac.at>
2024-01-03 18:54:29 +0100

    add license info to all source files

--- eomaps/ne_features.py
+++ eomaps/ne_features.py
@@ -1,3 +1,8 @@
+# Copyright EOmaps Contributors
+#
+# This file is part of EOmaps and is released under the BSD 3-clause license.
+# See LICENSE in the root of the repository for full licensing details.
+
 """Classes to fetch and draw NaturalEarth features."""
 
 import logging
