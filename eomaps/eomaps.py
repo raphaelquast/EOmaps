fix clipping issue with `m.get_extent` (crs bounds are given in the units of the crs)

eomaps/eomaps.py
@@ -2801,11 +2801,21 @@ class Maps(metaclass=_MapsMeta):
         extent : The extent in the given crs (x0, x1, y0, y1).
 
         """
-        bnds = self._crs_boundary_bounds
 
         # fast track if plot-crs is requested
         if crs == self.crs_plot:
             x0, x1, y0, y1 = (*self.ax.get_xlim(), *self.ax.get_ylim())
+
+            bnds = self._crs_boundary_bounds
+            # clip the map-extent with respect to the boundary bounds
+            # (to avoid returning values outside the crs bounds)
+            try:
+                x0, x1 = np.clip([x0, x1], bnds[0], bnds[2])
+                y0, y1 = np.clip([y0, y1], bnds[1], bnds[3])
+            except Exception:
+                _log.debug(
+                    "EOmaps: Error while trying to clip map extent", exc_info=True
+                )
         else:
             if crs is not None:
                 crs = self._get_cartopy_crs(crs)
@@ -2813,14 +2823,6 @@ class Maps(metaclass=_MapsMeta):
                 crs = self._get_cartopy_crs(4326)
 
             x0, x1, y0, y1 = self.ax.get_extent(crs=crs)
-
-        # clip the map-extent with respect to the boundary bounds
-        # (to avoid returning values outside the crs bounds)
-        try:
-            x0, x1 = np.clip([x0, x1], bnds[0], bnds[2])
-            y0, y1 = np.clip([y0, y1], bnds[1], bnds[3])
-        except Exception:
-            _log.debug("EOmaps: Error while trying to clip map extent", exc_info=True)
 
         return x0, x1, y0, y1
 