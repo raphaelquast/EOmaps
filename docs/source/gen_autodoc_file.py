1c721625ab47d563c568a89b1aaa9e21d690574c
Raphael Quast <raphael.quast@geo.tuwien.ac.at>
2024-08-24 19:56:38 +0200

    fix sphinx warning "Summarised items should not include the current module"

--- docs/source/gen_autodoc_file.py
+++ docs/source/gen_autodoc_file.py
@@ -122,7 +122,7 @@ def make_feature_toctree_file():
 
     s += get_autosummary(
         "eomaps.widgets",
-        [i for i in get_members(widgets) if not i.rsplit(".", 1)[-1][0].islower()],
+        [i.rsplit(".", 1)[1] for i in get_members(widgets) if not i.rsplit(".", 1)[-1][0].islower()],
     )
 
     basepath = Path(__file__).parent
