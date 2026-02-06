8a13920ccc0dad0cfc02520489038d8bb6433bda
Raphael Quast <raphael.quast@geo.tuwien.ac.at>
2026-02-06 22:55:06 +0100

    make sure all maps-objects get to the same _edit_annotations property

--- eomaps/mixins/tools_mixin.py
+++ eomaps/mixins/tools_mixin.py
@@ -13,13 +13,17 @@ class ToolsMixin:
     def __init__(self, *args, **kwargs):
         if self.parent == self:
             self.util = Utilities(self)
-            self._edit_annotations = AnnotationEditor(self)
+            self.__edit_annotations = AnnotationEditor(self)
         else:
             self.util = self.parent.util
 
         super().__init__(*args, **kwargs)
 
         self.draw = ShapeDrawer(weakref.proxy(self))
+
+    @property
+    def _edit_annotations(self):
+        return self.parent._ToolsMixin__edit_annotations
 
     @wraps(AnnotationEditor.__call__)
     def edit_annotations(self, b=True, **kwargs):
