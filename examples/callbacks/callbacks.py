4ad85a0c01198ff1576f6a0a5ac1a4549f58a00e
Raphael Quast <raphael.quast@geo.tuwien.ac.at>
2026-04-19 20:57:08 +0200

    update examples

--- examples/callbacks/callbacks.py
+++ examples/callbacks/callbacks.py
@@ -13,13 +13,14 @@ data = pd.DataFrame(
 
 # --------- initialize a Maps object and plot a basic map
 m = Maps(crs=3035, figsize=(10, 8))
-m.set_data(data=data, x="lon", y="lat", crs=4326)
-m.ax.set_title("A clickable widget!")
-m.set_shape.rectangles()
-
-m.set_classify_specs(scheme="EqualInterval", k=5)
 m.add_feature.preset.coastline()
 m.add_feature.preset.ocean()
+m.add_title("A clickable widget!")
+
+
+m.set_data(data=data, x="lon", y="lat", crs=4326)
+m.set_shape.rectangles()
+m.set_classify.EqualInterval(k=5)
 m.plot_map()
 
 # add some static text
@@ -38,6 +39,94 @@ m.add_text(
     fontweight="bold",
     bbox=dict(facecolor="w", alpha=0.75),
 )
+
+# add some dynamic text that will be updated
+txt = m.add_text(
+    0.5,
+    0.35,
+    "You clicked on 0 pixels so far",
+    fontsize=15,
+    horizontalalignment="center",
+    verticalalignment="top",
+    color="w",
+    fontweight="bold",
+    zorder=99,
+)
+
+txt2 = m.add_text(
+    0.18,
+    0.9,
+    "   lon    /    lat " + "\n",
+    fontsize=12,
+    horizontalalignment="right",
+    verticalalignment="top",
+    fontweight="bold",
+    zorder=99,
+)
+
+
+# add a colorbar
+m.add_colorbar(hist_bins="bins", label="A classified dataset")
+m.add_logo()
+
+m.apply_layout(
+    {
+        "figsize": [10.0, 8.0],
+        "0_map": [0.04375, 0.27717, 0.9125, 0.69566],
+        "1_cb": [0.01, 0.0, 0.98, 0.23377],
+        "1_cb_histogram_size": 0.8,
+        "2_logo": [0.825, 0.29688, 0.12, 0.06188],
+    }
+)
+
+
+### callback to save all values to a global "picked_vals" dict
+picked_vals = {}
+
+
+def save_picked_vals(event):
+    picked_vals.setdefault("ID", []).append([event.ID])
+    picked_vals.setdefault("val", []).append([event.val])
+
+
+def update_info_text(event, m):
+    # update the text that indicates how many pixels we've clicked
+    nvals = len(picked_vals["ID"])
+    txt.set_text(
+        f"You clicked on {nvals} pixel"
+        + ("s" if nvals > 1 else "")
+        + "!\n... and the "
+        + ("average " if nvals > 1 else "")
+        + f"value is {np.mean(picked_vals['val']):.3f}"
+    )
+
+    # update the list of lon/lat coordinates on the top left of the figure
+    d = m.data_specs.data.loc[event.ID]
+    lonlat_list = txt2.get_text().splitlines()
+    if len(lonlat_list) > 10:
+        lonlat_txt = lonlat_list[0] + "\n" + "\n".join(lonlat_list[-10:]) + "\n"
+    else:
+        lonlat_txt = txt2.get_text()
+    txt2.set_text(lonlat_txt + f"{d['lon']:.2f}  /  {d['lat']:.2f}" + "\n")
+
+
+def add_pick_marker(event, m):
+    # plot a marker at the pixel-position
+    (l,) = m.ax.plot(*event.pos, marker="*", animated=True)
+    # add the custom marker to the blit-manager!
+    m.add_artist(l)
+
+    # print the value at the pixel-position
+    # use a low zorder so the text will be drawn below the temporary annotations
+    m.add_text(
+        event.pos[0],
+        event.pos[1] - 150000,
+        f"{event.val:.2f}",
+        horizontalalignment="center",
+        verticalalignment="bottom",
+        color=l.get_color(),
+        zorder=1,
+    )
 
 
 # --------- attach pre-defined CALLBACK functions ---------
@@ -59,8 +148,7 @@ m.cb.pick.attach.annotate(
     bbox=dict(boxstyle="round", fc="w", alpha=0.75),
     zorder=999,
 )
-### save all picked values to a dict accessible via m.cb.get.picked_vals
-m.cb.pick.attach.get_values(button=1)
+
 
 ### add a permanent marker if you right-click on a pixel
 m.cb.pick.attach.mark(
@@ -72,6 +160,16 @@ m.cb.pick.attach.mark(
     shape="rectangles",
     zorder=1,
 )
+
+
+m.cb.pick.attach(save_picked_vals, button=1)
+m.cb.pick.attach(update_info_text, button=1, m=m)
+
+### remove all permanent markers and annotations if you middle-click anywhere on the map
+m.cb.pick.attach.clear_annotations(button=2)
+m.cb.pick.attach.clear_markers(button=2)
+
+m.cb.pick.attach(add_pick_marker, button=3, m=m)
 
 
 ### add a customized permanent annotation if you right-click on a pixel
@@ -88,97 +186,9 @@ m.cb.pick.attach.annotate(
     zorder=2,  # use zorder=2 to put the annotations on top of the markers
 )
 
-### remove all permanent markers and annotations if you middle-click anywhere on the map
-m.cb.pick.attach.clear_annotations(button=2)
-m.cb.pick.attach.clear_markers(button=2)
-
-# --------- define a custom callback to update some text to the map
-# (use a high zorder to draw the texts above all other things)
-txt = m.add_text(
-    0.5,
-    0.35,
-    "You clicked on 0 pixels so far",
-    fontsize=15,
-    horizontalalignment="center",
-    verticalalignment="top",
-    color="w",
-    fontweight="bold",
-    animated=True,
-    zorder=99,
-)
-txt2 = m.add_text(
-    0.18,
-    0.9,
-    "   lon    /    lat " + "\n",
-    fontsize=12,
-    horizontalalignment="right",
-    verticalalignment="top",
-    fontweight="bold",
-    animated=True,
-    zorder=99,
-)
-
-
-def cb1(m, pos, ID, val, **kwargs):
-    # update the text that indicates how many pixels we've clicked
-    nvals = len(m.cb.pick.get.picked_vals["ID"])
-    txt.set_text(
-        f"You clicked on {nvals} pixel"
-        + ("s" if nvals > 1 else "")
-        + "!\n... and the "
-        + ("average " if nvals > 1 else "")
-        + f"value is {np.mean(m.cb.pick.get.picked_vals['val']):.3f}"
-    )
-
-    # update the list of lon/lat coordinates on the top left of the figure
-    d = m.data_specs.data.loc[ID]
-    lonlat_list = txt2.get_text().splitlines()
-    if len(lonlat_list) > 10:
-        lonlat_txt = lonlat_list[0] + "\n" + "\n".join(lonlat_list[-10:]) + "\n"
-    else:
-        lonlat_txt = txt2.get_text()
-    txt2.set_text(lonlat_txt + f"{d['lon']:.2f}  /  {d['lat']:.2f}" + "\n")
-
-
-m.cb.pick.attach(cb1, button=1, m=m)
-
-
-def cb2(m, pos, val, **kwargs):
-    # plot a marker at the pixel-position
-    (l,) = m.ax.plot(*pos, marker="*", animated=True)
-    # add the custom marker to the blit-manager!
-    m.add_artist(l)
-
-    # print the value at the pixel-position
-    # use a low zorder so the text will be drawn below the temporary annotations
-    m.add_text(
-        pos[0],
-        pos[1] - 150000,
-        f"{val:.2f}",
-        horizontalalignment="center",
-        verticalalignment="bottom",
-        color=l.get_color(),
-        zorder=1,
-    )
-
-
-m.cb.pick.attach(cb2, button=3, m=m)
 
 # add a "target-indicator" on mouse-movement
 m.cb.move.attach.mark(fc="r", ec="none", radius=10000, shape="geod_circles")
 m.cb.move.attach.mark(fc="none", ec="r", radius=50000, shape="geod_circles")
 
-# add a colorbar
-m.add_colorbar(hist_bins="bins", label="A classified dataset")
-m.add_logo()
-
-m.apply_layout(
-    {
-        "figsize": [10.0, 8.0],
-        "0_map": [0.04375, 0.27717, 0.9125, 0.69566],
-        "1_cb": [0.01, 0.0, 0.98, 0.23377],
-        "1_cb_histogram_size": 0.8,
-        "2_logo": [0.825, 0.29688, 0.12, 0.06188],
-    }
-)
 m.show()
